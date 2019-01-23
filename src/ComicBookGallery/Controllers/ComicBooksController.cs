224845137d80ab20edf25a0a0e4dff96a9cde53a
Krissy Dukes <krissydotnet@yahoo.com>
2019-01-23 08:17:07 -0500

    Update

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,16 +8,5 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
-        {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller!");
-
-        }
-            
-        
     }
 }
