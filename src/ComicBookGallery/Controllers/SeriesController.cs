a0a28b806d16c203e4298b64a2b5ae578fa6eec4
Krissy Dukes <krissydotnet@yahoo.com>
2019-01-26 08:33:15 -0500

    Added Series Detail

--- src/ComicBookGallery/Controllers/SeriesController.cs
+++ src/ComicBookGallery/Controllers/SeriesController.cs
@@ -21,5 +21,14 @@ namespace ComicBookGallery.Controllers
             var series = _seriesRepository.GetSeries();
             return View(series);
         }
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var series = _seriesRepository.GetSeriesDetail((int)id);
+            return View(series);
+        }
     }
 }
